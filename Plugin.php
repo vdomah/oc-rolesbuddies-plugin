e69fe282ff7abf981da2b9a30e38313d7d7d5210
Artem Rybachuk <firebatt@ukr.net>
2020-05-29 15:53:17 +0300

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,21 @@
+<?php namespace Vdomah\RolesBuddies;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = ['Lovata.Buddies', 'Vdomah.Roles'];
+
+    public function registerComponents()
+    {
+        return [
+
+        ];
+    }
+
+    public function registerSettings()
+    {
+        return [
+        ];
+    }
+}
